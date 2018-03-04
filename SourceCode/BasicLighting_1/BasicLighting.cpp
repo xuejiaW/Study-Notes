d67a1700bd2fb862ac51c6551cf50979cfb27787
xuejia.wang <wxjwangxuejia@outlook.com>
2018-03-04 16:41:50 +0800

    feat: Add UP/DOWN movement in camera.h
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

--- SourceCode/BasicLighting_1/BasicLighting.cpp
+++ SourceCode/BasicLighting_1/BasicLighting.cpp
@@ -225,25 +225,19 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 void ProcessKeyInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
 		glfwSetWindowShouldClose(window, true);
-	}
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-	{
 		camera.ProcessKeyboard(FORWARD, deltaTime);
-	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-	{
 		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	}
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-	{
 		camera.ProcessKeyboard(LEFT, deltaTime);
-	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-	{
 		camera.ProcessKeyboard(RIGHT, deltaTime);
-	}
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		camera.ProcessKeyboard(UP, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		camera.ProcessKeyboard(DOWN, deltaTime);
 }
 
 void CalculateDeletaTime()
