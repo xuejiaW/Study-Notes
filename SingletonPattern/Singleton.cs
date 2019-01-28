a6b96730d1ee54b35c386098f6b2377d3fae3014
xuejia.wang <wxjwangxuejia@outlook.com>
2019-01-29 00:06:05 +0800

    feat: add SingletonPattern
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static object lockObj = new object();
+        private static Singleton instance = null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObj)
+                        if (instance == null)
+                            instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            Console.WriteLine("Constructor the Singleton");
+        }
+    }
+}
