2af1f17e28d3254608a8263afddd937a72794b7a
xuejia.wang <wxjwangxuejia@outlook.com>
2019-09-20 18:08:59 +0100

    feat: create Python-100-Days folder and create Day01
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ Python-100-Days/Day01.py
@@ -0,0 +1,19 @@
+print('hello,world!')
+
+# show The Zen Of Python
+import this
+
+# Draw sth
+import turtle
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
