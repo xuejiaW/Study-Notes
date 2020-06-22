f50c1c7457db45208f213420b49ec2087198424e
xuejia.wang <wxjwangxuejia@outlook.com>
2020-06-22 17:56:35 +0100

    feat: add dp solution for maximum-subarray
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

--- LeetCode/53.maximum-subarray.h
+++ LeetCode/53.maximum-subarray.h
@@ -3,7 +3,7 @@
 // 难度：简单
 
 // Tags:
-// array | divide-and-conquer
+// array | divide-and-conquer | dynamic programming
 
 // 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 
@@ -12,7 +12,6 @@
 // 输入: [-2,1,-3,4,-1,2,1,-5,4],
 // 输出: 6
 // 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
-
 
 #include <iostream>
 #include <vector>
@@ -77,3 +76,20 @@ int getMaxCrossArray(vector<int> &nums, int begin, int end)
 
     return maxLeftPartSum + maxRightPartSum;
 }
+
+// // Version: dynamic programmming
+// int maxSubArray(vector<int> &nums)
+// {
+//     if (nums.empty())
+//         return 0;
+//     vector<int> temp = nums;
+
+//     int result = nums[0];
+
+//     for (int i = 1; i != nums.size(); ++i)
+//     {
+//         temp[i] += max(temp[i - 1], 0);
+//         result = max(temp[i], result);
+//     }
+//     return result;
+// }
