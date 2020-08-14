fce50cf4fc07fa93df370ff7aabaad45a5c00174
xuejia.wang <wxjwangxuejia@outlook.com>
2020-08-14 20:36:50 +0800

    feat: add texture courses
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ LearnOpenGL_VSCode/src/3.Texture/main.cpp
@@ -0,0 +1,76 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb_image.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "../SHADER.h"
+#include <iostream>
+
+using namespace std;
+
+static int windowWidth = 800;
+static int windowHeight = 800;
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    GLfloat vertices[] =
+        {
+            -0.5f, -0.5f, 0, 0, 0,
+            0.5f, -0.5f, 0, 1, 0,
+            0.5f, 0.5f, 0, 1, 1,
+            -0.5f, 0.5f, 0, 0, 1};
+
+    GLuint index[] =
+        {
+            0, 1, 2,
+            0, 2, 3};
+
+    GLuint VAO, VBO, EBO;
+
+    // Generate buffer
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // Bind buffer and load data
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+    // Unbind buffer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    Shader shader("triangle.vert", "triangle.frag");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        shader.Use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+
+    return 0;
+}
