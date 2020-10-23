523cd8c90ad62450cceacfa22bc3bbae8d2af69b
wangxuejia <wangxuejia.wywk.cn>
2020-10-23 13:31:25 +0800

    feat: disable cameraController temporarily
    Signed-off-by: wangxuejia <wangxuejia.wywk.cn>

--- LearnOpenGL_VSCode/src/Framework/Components/CameraController.cpp
+++ LearnOpenGL_VSCode/src/Framework/Components/CameraController.cpp
@@ -13,27 +13,27 @@ CameraController::CameraController(Camera *target) : targetCamera(target)
 
 void CameraController::Update()
 {
-    glm::vec3 cameraPos = targetCamera->GetPosition();
-    glm::vec3 forward = targetCamera->GetForward();
-    float speed = 2.5f * Scene::GetInstance()->GetFrameTime();
-    if (Input::GetInstance()->GetKey(GLFW_KEY_W))
-    {
-        cameraPos += speed * forward;
-    }
-    if (Input::GetInstance()->GetKey(GLFW_KEY_A))
-    {
-        cout << "Press W" << endl;
-    }
-    if (Input::GetInstance()->GetKey(GLFW_KEY_S))
-    {
-        cameraPos -= speed * forward;
-        cout << "Press S" << endl;
-    }
-    if (Input::GetInstance()->GetKey(GLFW_KEY_D))
-    {
-        cout << "Press W" << endl;
-    }
-    targetCamera->SetPosition(cameraPos);
+    // glm::vec3 cameraPos = targetCamera->GetPosition();
+    // glm::vec3 forward = targetCamera->GetForward();
+    // float speed = 2.5f * Scene::GetInstance()->GetFrameTime();
+    // if (Input::GetInstance()->GetKey(GLFW_KEY_W))
+    // {
+    //     cameraPos += speed * forward;
+    // }
+    // if (Input::GetInstance()->GetKey(GLFW_KEY_A))
+    // {
+    //     cout << "Press W" << endl;
+    // }
+    // if (Input::GetInstance()->GetKey(GLFW_KEY_S))
+    // {
+    //     cameraPos -= speed * forward;
+    //     cout << "Press S" << endl;
+    // }
+    // if (Input::GetInstance()->GetKey(GLFW_KEY_D))
+    // {
+    //     cout << "Press W" << endl;
+    // }
+    // targetCamera->SetPosition(cameraPos);
 }
 
 CameraController::~CameraController()
