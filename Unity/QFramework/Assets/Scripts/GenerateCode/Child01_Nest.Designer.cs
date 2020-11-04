647b357d1191c32d1ad7afbbfa7dcbfeb8ae0a83
wangxuejia <wangxuejia.wywk.cn>
2020-11-04 17:46:45 +0800

    feat: add Generate Code
    Signed-off-by: wangxuejia <wangxuejia.wywk.cn>

---
+++ Unity/QFramework/Assets/Scripts/GenerateCode/Child01_Nest.Designer.cs
@@ -0,0 +1,14 @@
+// Generate Id:0c52eb30-3202-4270-b6ae-8a7e07cb16aa
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFramework.Example
+{
+	public partial class Child01_Nest
+	{
+		public UnityEngine.Transform Child01_Child01;
+		public UnityEngine.Transform Child02_Child01;
+
+	}
+}
---
+++ Unity/QFramework/Assets/Scripts/GenerateCode/Root.Designer.cs
@@ -0,0 +1,16 @@
+// Generate Id:ff5d96b7-195d-4303-a6fa-6e9f6b8bf0bc
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFramework.Example
+{
+	public partial class Root
+	{
+		public UnityEngine.UI.Image Child01;
+		public UnityEngine.Transform Child01_Child01;
+		public Transform Child02_Child01;
+		public Transform Child02;
+
+	}
+}
