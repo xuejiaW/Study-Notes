259c85ebfe4439de6f03354536fe58263ab1e1df
wangxuejia <wxjwangxuejia@outlook.com>
2021-02-11 23:58:09 +0800

    feat: add shadow pcf to solve shadow aliasing
    Signed-off-by: wangxuejia <wxjwangxuejia@outlook.com>

--- LearnOpenGL_VSCode/src/Framework/Shaders/shadow.fs
+++ LearnOpenGL_VSCode/src/Framework/Shaders/shadow.fs
@@ -26,12 +26,23 @@ float ShaderCalculation(vec4 FragPosLightSpace, vec3 normal, vec3 lightDir)
     projCoords = projCoords * 0.5 + 0.5;
 
     if (projCoords.z > 1.0) // larger than the far clipping
-        return 0.0f; 
-    float closetDepth = texture(depthMap, projCoords.xy).r;
+        return 0.0f;
 
-    float bias = max(0.05 * (1 - dot(normal, lightDir)), 0.01);
+    float shadow = 0.0f;
+    vec2 texelSize = 1.0 / textureSize(depthMap, 0);
+
     float currentDepth = projCoords.z;
-    return currentDepth > closetDepth + bias ? 1.0 : 0.0;
+    float bias = max(0.05 * (1 - dot(normal, lightDir)), 0.01);
+    for (int x = -1; x <= 1; ++x)
+    {
+        for (int y = -1; y <= 1; ++y)
+        {
+            float closetDepth = texture(depthMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += (currentDepth > closetDepth + bias ? 1.0 : 0.0);
+        }
+    }
+
+    return shadow /= 9.0;
 }
 
 void main()
