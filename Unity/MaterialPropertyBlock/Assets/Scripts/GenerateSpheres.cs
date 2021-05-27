15bd1184cb448e931574b05b9938be71cda49836
wangxuejia <wxjwangxuejia@outlook.com>
2021-05-28 00:26:04 +0800

    feat: add test for MaterialPropertyBlock
    Signed-off-by: wangxuejia <wxjwangxuejia@outlook.com>

---
+++ Unity/MaterialPropertyBlock/Assets/Scripts/GenerateSpheres.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateSpheres : MonoBehaviour
+{
+    private float gap = 1.5f;
+    public Material colorMaterial = null;
+
+    private void Start()
+    {
+        for (int row = -25; row != 25; ++row)
+        {
+            for (int col = -25; col != 25; ++col)
+            {
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.AddComponent<ModifyColor>();
+                sphere.GetComponent<MeshRenderer>().material = colorMaterial;
+                sphere.transform.localScale = new Vector3(gap, gap, gap);
+                sphere.transform.position = new Vector3(gap * row, 0, gap * col);
+            }
+        }
+    }
+}
---
+++ Unity/MaterialPropertyBlock/Assets/Scripts/ModifyColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyColor : MonoBehaviour
+{
+    private MeshRenderer meshRenderer = null;
+
+    private MaterialPropertyBlock propertyBlock = null;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        Color color = new Color(Mathf.Sin(Time.time), Mathf.Cos(Time.time), 0, 1);
+
+        // // Using Material
+        // meshRenderer.material.color = color;
+
+        // // Using MaterialPropertyBlock
+        propertyBlock.SetColor("_Color", color);
+        meshRenderer.SetPropertyBlock(propertyBlock);
+    }
+}
+
